73db2a5096ec85ebe58c417f9d34fb9fef5d03cd
tokio-takeda <defeated.diabolical.pitch@gmail.com>
2017-06-03 18:54:18 +0900

    first version.
    It's simple.
    only toggle status on/off.
    
     Changes to be committed:
    	modified:   src/main/js/web/flopsycontroller.js

--- src/main/js/web/flopsycontroller.js
+++ src/main/js/web/flopsycontroller.js
@@ -1,8 +1,13 @@
+var dash_button = require('node-dash-button');
 var hue = require("node-hue-api"),
     HueApi = hue.HueApi,
     lightState = hue.lightState;
-
 var bridge = require("../../../main/resources/bridge");
+
+var dash = dash_button("34:d2:70:4b:a3:1b", null, null, 'all');
+
+var api = new HueApi(bridge.ip, bridge.username),
+    state = lightState.create();
 
 var displayResult = function(result) {
     console.log(result);
@@ -12,24 +17,23 @@ var displayError = function(err) {
     console.error(err);
 };
 
-var api = new HueApi(bridge.ip, bridge.username),
-    state = lightState.create();
+dash.on("detected", function (){
+    api.lightStatus(2)
+        .then(function(result){
+            displayResult(result);
 
-api.lightStatus(2)
-    .then(function(result){
-        displayResult(result);
-
-        if(result.state.on) {
-            api.setLightState(2, state.off())
-                .then(displayResult)
-                .fail(displayError)
-                .done();
-        } else {
-            api.setLightState(2, state.on())
-            	.then(displayResult)
-                .fail(displayError)
-                .done();
-        }
-    })
-    .fail(displayError)
-    .done();
+            if(result.state.on) {
+                api.setLightState(2, state.off())
+                    .then(displayResult)
+                    .fail(displayError)
+                    .done();
+            } else {
+                api.setLightState(2, state.on())
+                	.then(displayResult)
+                    .fail(displayError)
+                    .done();
+            }
+        })
+        .fail(displayError)
+        .done();
+});
